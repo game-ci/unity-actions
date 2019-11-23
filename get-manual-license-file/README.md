7aae73d314f9b7c8de94c9c9d8b69b6c9f444c39
Webber <webber@takken.io>
2019-11-23 13:32:32 +0100

    Refactor request activation file action to follow good practices

--- get-manual-license-file/README.md
+++
@@ -1,7 +0,0 @@
-# Get manual license file
-
-Acquires manual license file for activating CI license.
-
-## Usage
-
-Unknown so far
---
+++ request-manual-activation-file/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Request the manual activation file for activating unity personal
+# Expected output file: "Unity_v${{ unity-tag }}.alf"
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
+  /opt/Unity/Editor/Unity \
+    -batchmode \
+    -nographics \
+    -logFile /dev/stdout \
+    -quit \
+    -createManualActivationFile
+
+# Output the resulting file as an output variable (Strategy 1)
+ACTIVATION_FILE=$(cat Unity_v2019.2.11f1.alf)
+echo "Use this file for manual activation and add it to \$UNITY_LICENSE variabe."
+echo "::add-mask::$ACTIVATION_FILE"
+echo "::set-output name=activationFile::$ACTIVATION_FILE"
+
+# Output the resulting file by copying (Strategy 2)
+cp Unity_v2019.2.11f1.alf $HOME/Unity_v2019.2.11f1.alf
