5094a8aaf456eb4096310f56e532c4c06d11536c
Webber <webber@takken.io>
2019-11-24 23:31:28 +0100

    Cleanup comments

--- request-activation/entrypoint.sh
+++ request-activation/entrypoint.sh
@@ -9,9 +9,12 @@ if [[ -n "$UNITY_LICENSE" ]]; then
   # Note that this is the ONLY WAY for PERSONAL LICENSES in 2019.
   #   * See for more details: https://gitlab.com/gableroux/unity3d-gitlab-ci-example/issues/5#note_72815478
   #
-  # The license file can be found here:
-  #   * Windows:   C:/ProgramData/Unity/Unity_lic.ulf
-  #   * MacOS:     /Library/Application Support/Unity/Unity_lic.ulf
+  # The license file can be acquired in two possible ways:
+  #   * Utilize `webbertakken/unity-actions/request-manual-activation-file`
+  #   * Copy from your local machine (may be unstable due to different server specs)
+  #       - Windows:   C:/ProgramData/Unity/Unity_lic.ulf
+  #       - MacOS:     /Library/Application Support/Unity/Unity_lic.ulf
+  #       - Linux:     -
   #
   # CLI arguments reference: https://docs.unity3d.com/Manual/CommandLineArguments.html
 
