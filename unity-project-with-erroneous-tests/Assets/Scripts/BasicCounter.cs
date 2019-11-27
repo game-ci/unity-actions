eabb7275c910ee6ecc26ee189012e2f6dc8d5084
Webber <webber@takken.io>
2019-11-27 23:01:08 +0100

    Add project with erroneous tests

---
+++ unity-project-with-erroneous-tests/Assets/Scripts/BasicCounter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class BasicCounter
+{
+  public const int MaxCount = 10;
+
+  public BasicCounter(int count = 0)
+  {
+    Count = count;
+  }
+
+  public void Increment()
+  {
+    Count = Math.Min(MaxCount, Count + 1);
+  }
+
+  public int Count { get; private set; }
+}
